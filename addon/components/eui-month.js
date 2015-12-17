64686cc1f666091b4e27518c2c9d7c77655dec83
palimpsests <zach.zibrat@locusenergy.com>
2015-12-16 22:15:30 -0800

    improves math

--- addon/components/eui-month.js
+++ addon/components/eui-month.js
@@ -2,12 +2,15 @@ import Ember from 'ember';
 import layout from '../templates/components/eui-month';
 import moment from 'moment';
 
+const { computed } = Ember;
+const DAY_COUNT = 42;
+
 export default Ember.Component.extend({
   layout: layout,
   tagName: 'ol',
   classNames: 'eui-month',
-  //
-  // month: null,
+
+  month: null,
   //
   // selection: [],
   // disabledDates: [],
@@ -16,32 +19,23 @@ export default Ember.Component.extend({
   //
   // onSelect: null,
   //
-  // setup: Ember.on('init', function() {
-  //   Ember.assert('You must provide a month to eui-month', this.get('month'));
-  // }),
+  setup: Ember.on('init', function() {
+    Ember.assert('You must provide a month to eui-month', this.get('month'));
+  }),
 
-  days: Ember.computed('month', function() {
+  days: computed('month', function() {
     const monthStart = moment(this.get('month')).startOf('month');
-    const monthEnd = moment(this.get('month')).endOf('month');
-    const daysInMonth = monthStart.daysInMonth();
     const dayOfWeek = monthStart.day();
     const slots = [];
 
-    const leftOver = 42 - daysInMonth - dayOfWeek;
-
-    for (let i = 0; i < dayOfWeek; i++) {
-      slots.push(monthStart.clone().subtract(dayOfWeek - i, 'days'));
-    }
-
-    for (let i = 1; i <= daysInMonth; i++) {
-      const slot = moment(monthStart).date(i);
-      slots.push(slot);
-    }
-
-    for (let i = 0; i < leftOver; i++) {
-      slots.push(monthEnd.clone().add(i + 1, 'days'));
+    for (let i = -dayOfWeek; i < DAY_COUNT - dayOfWeek; i++) {
+      slots.push(monthStart.clone().add(i, 'days'))
     }
 
     return slots;
   })
 });
+
+function sortDate(a, b) {
+  return a.unix() - b.unix();
+}
--- addon/tests/page-object.js
+++ addon/tests/page-object.js
@@ -1,30 +1,31 @@
 export default class MonthPageObject {
   constructor(env) {
     this.env = env;
+    this.$ = this.env.$;
   }
 
   daysCount() {
-    return this.env.$('.eui-month--day').length;
+    return this.$('.eui-month--day').length;
   }
 
   slotCount() {
-    return this.env.$('.eui-month--slot').length;
+    return this.$('.eui-month--slot').length;
   }
 
   emptySlotCount() {
-    return this.env.$('.eui-month--empty').length;
+    return this.$('.eui-month--empty').length;
   }
 
   deactivatedSlotCount() {
-    return this.env.$('.eui-month--disabled').length;
+    return this.$('.eui-month--disabled').length;
   }
 
   calendar() {
-    return this.env.$('.eui-month--slot').map(trimText).toArray();
+    return this.$('.eui-month--slot').map(trimText).toArray();
   }
 
   headers() {
-    return this.env.$('.eui-nameofday').map(trimText).toArray();
+    return this.$('.eui-nameofday').map(trimText).toArray();
   }
 }
 
