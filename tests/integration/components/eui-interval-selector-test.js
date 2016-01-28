897c66f0f287a00f658187448c6f9426d9cda525
zigahertz <zach.zibrat@locusenergy.com>
2016-01-28 11:27:37 -0800

    wip

--- tests/integration/components/eui-interval-selector-test.js
+++ tests/integration/components/eui-interval-selector-test.js
@@ -5,20 +5,28 @@ moduleForComponent('eui-interval-selector', 'Integration | Component | eui inter
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+// tests:
 
-  this.render(hbs`{{eui-interval-selector}}`);
+// can zoom
+// previous
+// next
 
-  assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#eui-interval-selector}}
-      template block text
-    {{/eui-interval-selector}}
-  `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+// test('it renders', function(assert) {
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+//
+//   this.render(hbs`{{eui-interval-selector}}`);
+//
+//   assert.equal(this.$().text().trim(), '');
+//
+//   // Template block usage:" + EOL +
+//   this.render(hbs`
+//     {{#eui-interval-selector}}
+//       template block text
+//     {{/eui-interval-selector}}
+//   `);
+//
+//   assert.equal(this.$().text().trim(), 'template block text');
+// });
